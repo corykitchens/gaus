a3cd3e51491b1de40ea2e9c3eeb329eeb0e33499
Cory Kitchens <corykitchens@gmail.com>
2016-10-24 13:28:52 -0700

    Delete unused js scripts

--- public/js/navtabs.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    colors = ['red', 'blue', 'green', 'purple', 'orange'];
-    var path = window.document.location.pathname.split('/');
-    current_location = path[path.length-1];
-    var idx = $('.nav-item[name='+ current_location + ']').index();
-    $('.nav-item[name='+ current_location + ']').css('border-top', '5px solid ' + colors[idx]);
-})();
