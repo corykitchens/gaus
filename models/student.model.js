7e24da0cc85c1bbf4bf1ef4c98a5b17c71846ac6
Cory Kitchens <corykitchens@gmail.com>
2016-10-24 15:31:18 -0700

    Seperate schema definitions

--- models/student.model.js
+++ models/student.model.js
@@ -1,6 +1,52 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+/**
+* TODO
+* use 0 as default value for each individual test score?
+* use setdefaultsonInsert to true so that a json object
+* can be returned from the frontend
+*/
+
+const waisSchema = new Schema({
+  vc:   Number,
+  pri:  Number,
+  wmi:  Number,
+  ps:   Number,
+  fsiq: Number,
+  gai:  Number,
+  final_score: Number,
+});
+
+const microcogSchema = new Schema({
+  gcf:    Number,
+  gcp:    Number,
+  ips:    Number,
+  attn:   Number,
+  reas:   Number,
+  mem:    Number,
+  spat:   Number,
+  final_score: Number,
+});
+
+const wiatSchema = new Schema({
+  lc: Number,
+  oe: Number,
+  rc: Number,
+  wr: Number,
+  pd: Number,
+  orf: Number,
+  sc: Number,
+  ec: Number,
+  sp: Number,
+  mps: Number,
+  no: Number,
+  mfa: Number,
+  mfs: Number,
+  mfm: Number,
+  final_score: Number,
+})
+
 const studentSchema = new Schema({
   fname: String,
   lname: String,
@@ -8,42 +54,9 @@ const studentSchema = new Schema({
   gender: String,
   created_at: Date,
   evaluations: {
-    wais: {
-      vc: Number,
-      pri: Number,
-      wmi: Number,
-      ps: Number,
-      fsiq: Number,
-      gai: Number,
-      final_score: Number,
-    },
-    micro_cog: {
-      gcf: Number,
-      gcp: Number,
-      ips: Number,
-      attn: Number,
-      reas: Number,
-      mem: Number,
-      spat: Number,
-      final_score: Number,
-    },
-    wiat: {
-      lc: Number,
-      oe: Number,
-      rc: Number,
-      wr: Number,
-      pd: Number,
-      orf: Number,
-      sc: Number,
-      ec: Number,
-      sp: Number,
-      mps: Number,
-      no: Number,
-      mfa: Number,
-      mfs: Number,
-      mfm: Number,
-      final_score: Number,
-    },
+    wais: [waisSchema],
+    micro_cog: [microcogSchema],
+    wiat: [wiatSchema],
   }
 });
 
