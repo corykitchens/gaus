96e350cb21377ca65d0c05ae63f37416fa0b0f65
Cory Kitchens <corykitchens@gmail.com>
2016-10-24 13:26:33 -0700

    Use key/val data structure for evaluations

--- models/student.model.js
+++ models/student.model.js
@@ -7,50 +7,44 @@ const studentSchema = new Schema({
   csub_id: Number,
   gender: String,
   created_at: Date,
-  evaluations: [
-    {
-      wais: {
-        vc: Number,
-        pri: Number,
-        wmi: Number,
-        ps: Number,
-        fsiq: Number,
-        gai: Number,
-        final_score: Number,
-      }
+  evaluations: {
+    wais: {
+      vc: Number,
+      pri: Number,
+      wmi: Number,
+      ps: Number,
+      fsiq: Number,
+      gai: Number,
+      final_score: Number,
     },
-    {
-      micro_cog: {
-        gcf: Number,
-        gcp: Number,
-        ips: Number,
-        attn: Number,
-        reas: Number,
-        mem: Number,
-        spat: Number,
-        final_score: Number,
-      }
+    micro_cog: {
+      gcf: Number,
+      gcp: Number,
+      ips: Number,
+      attn: Number,
+      reas: Number,
+      mem: Number,
+      spat: Number,
+      final_score: Number,
     },
-    {
-      wiat: {
-        lc: Number,
-        oe: Number,
-        rc: Number,
-        wr: Number,
-        pd: Number,
-        orf: Number,
-        sc: Number,
-        ec: Number,
-        sp: Number,
-        mps: Number,
-        no: Number,
-        mfa: Number,
-        mfs: Number,
-        mfm: Number,
-        final_score: Number,
-      }
-    }
-  ]
+    wiat: {
+      lc: Number,
+      oe: Number,
+      rc: Number,
+      wr: Number,
+      pd: Number,
+      orf: Number,
+      sc: Number,
+      ec: Number,
+      sp: Number,
+      mps: Number,
+      no: Number,
+      mfa: Number,
+      mfs: Number,
+      mfm: Number,
+      final_score: Number,
+    },
+  }
 });
 
 const Student = mongoose.model('Student', studentSchema);
